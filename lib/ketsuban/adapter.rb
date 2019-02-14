912fbe5004fbaa5e8628bdabf6f14c655e3a9f34
oieioi <atsuinatsu.samuifuyu@gmail.com>
2019-02-14 14:54:11 +0900

    Get adapters dynamically

--- lib/ketsuban/adapter.rb
+++ lib/ketsuban/adapter.rb
@@ -3,13 +3,10 @@ require_relative './adapters/postgresql_adapter'
 module Ketsuban
   module Adapter
     def get_adapter(model_class)
-      case ActiveRecord::Base.connection.adapter_name.downcase
-      when 'postgresql'
-        PostgreSQLAdapter.new(model_class)
-      when 'mysql'
-      when 'sqlite3'
-      else raise 'Not Implemented Yet'
-      end
+      adapter_name = ActiveRecord::Base.connection.adapter_name
+      const_get("#{adapter_name}Adapter").new(model_class)
+    rescue => e
+      raise 'Not Found adapter', e
     end
     module_function :get_adapter
   end
